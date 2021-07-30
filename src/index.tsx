2f1cdbc910f4144ecd9c26197611c6b684c804ba
Василий Субботин <vs@smena.space>
2021-07-30 15:39:16 +0500

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React, {useEffect, useState} from 'react';
+import {render} from 'react-dom';
 
-const Application = () => (
-  <h1 data-testid="application">Application</h1>
-);
+const Application = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/items/12?q=sdf")
+      .then(response => response.json())
+      .then(data => {
+        setCount(data.item_id)
+      })
+  }, []);
+
+  return (
+    <div data-testid="application">
+      <h1 >Application</h1>
+      <p data-testid="clicked-count">You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
 
 render(<Application />, document.getElementById('app'));
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,10 @@
 import {test, expect} from '@playwright/test';
 
 test('basic test', async ({page}) => {
-  await page.goto('http://localhost:8000/docs');
-  const name = await page.innerText('data-testid=application');
-  expect(name).toBe('Application');
+  await page.goto('http://localhost:1234/');
+
+  await page.waitForResponse('http://localhost:8000/items/12?q=sdf');
+  const clickedCount = await page.innerText('data-testid=clicked-count');
+
+  expect(clickedCount).toBe('You clicked 12 times');
 });
